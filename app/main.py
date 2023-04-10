198b07ee95a3d613cd4b8df59f9cdb63dcae91e1
Mich Ochieng <michochieng@gmail.com>
2023-04-09 23:38:17 -0700

    Added FastAPI endpoints

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.model.model import predict, prependCircuit, convertTime
+
+app = FastAPI()
+
+class Inputs(BaseModel):
+    time: str
+    circuit: str
+
+class Outputs(BaseModel):
+    podium: bool
+
+# Health check
+@app.get('/')
+def home():
+    return {"Health": "Ok"}
+
+@app.post("/predict",response_model=Outputs)
+def _predict(payload: Inputs):
+    # Convert time and fix circuit name
+    time    = convertTime(payload.time)
+    circuit = prependCircuit(payload.circuit)
+    # Predict
+    podium  = predict(time=time,circuit=circuit)
+    return { "podium" : podium } 
