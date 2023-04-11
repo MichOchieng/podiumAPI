e1d475f5633e8ddc2124c680e1b7465088bb772d
Mich Ochieng <michochieng@gmail.com>
2023-04-10 18:58:25 -0700

    Fixes to port

--- app/main.py
+++ app/main.py
@@ -27,6 +27,6 @@ def _predict(payload: Inputs):
     podium  = predict(time=time,circuit=circuit)
     return { "podium" : podium } 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT",8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT",8000))
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
